e64672728f1fb92064f67d869c2bff38cc8d09b4
Jakub <kubiak.jakub01@gmail.com>
2022-09-22 21:58:09 +0200

    Init Problem111.py and provide draft of algoritm desciption

---
+++ Problem111.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 22-09-2022 21:52
+
+Problem 111: Primes with runs
+https://projecteuler.net/problem=111
+
+Algoritm description [draft]:
+    1. Found all primes numbers below sqrt(10^10)
+
+"""
+
+# import libraries
+import time
+
+
+
+# main solution function
+def solution():
+    pass
+
+if __name__ == "__main__":
+    #start_time = time.time()
+    solution()
+    #print("--- %s seconds ---" % (time.time() - start_time))
