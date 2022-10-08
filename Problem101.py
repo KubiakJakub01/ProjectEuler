c559f8628cc29f3939e8f0bb51c9c249a712eeb6
James Kubiak <kubaszek10@gmail.com>
2022-10-08 14:42:49 +0200

    Completion of the algorithm description

--- Problem101.py
+++ Problem101.py
@@ -7,22 +7,24 @@ https://projecteuler.net/problem=101
 
 @author: Jakub
 
-Algoritm description [draft]:
-    1. Create a list of points
-    2. Create a list of polynomials
-    3. Check if the polynomial is correct
-    4. END
+Algoritm description:
+    1. Loop x through degree of polynomial
+    2. Determine point of polynomial for current x
+    3. Create level_dict where under key 0 is list of points of polynomial
+        and under next keys are list of differences between two last elements from previous level
+        till x+1 where is the last element
+    4. Sum last elements from all lists in level_dict which is BOP for current x   
+    5. Sum all BOPs and return the result
+    6. END
 """
 
 # import libraries
 import time
-import numpy as np
 from collections import defaultdict
 
 # given coefficients:
 # 1 − n + n2 − n3 + n4 − n5 + n6 − n7 + n8 − n9 + n10
 COEFFICIENTS = [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1]
-TEST_COEFFICIENTS = [0, 0, 0, 1]
 N = len(COEFFICIENTS) - 1
 
 # polunomial function
@@ -57,3 +59,7 @@ if __name__ == "__main__":
     BOP = solution()
     print(f"BOP: {BOP} sum: {sum(BOP)}")
     print("--- %s seconds ---" % (time.time() - start_time))
+
+"""
+--- 0.0009975433349609375 seconds ---
+"""
