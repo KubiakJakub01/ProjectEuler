54137d4d5bd8de6ee9a659be61117dccaf3e351a
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-10-15 19:28:01 +0200

    Use reverse search to find the answer

--- Problem119.py
+++ Problem119.py
@@ -12,12 +12,27 @@ Algoritm description [Draft]:
 
 # import libraries
 import time
+import math
 
 # main solution function
 def solution():
-    pass
+    n = 1
+    number_list = []
+    # for every number in (2, 50) generate power if (2, 10)
+    for i in range(2, 100):
+        for j in range(2, 15):
+            n = i**j
+            if sum([int(x) for x in str(n)]) == i:
+                number_list.append(n)
+    return sorted(number_list)[29]
+                
+                
 
 if __name__ == "__main__":
-    # start_time = time.time()
-    solution()
-    # print(f"Solution found in {time.time() - start_time} seconds")
+    start_time = time.time()
+    print(solution())
+    print(f"Solution found in {time.time() - start_time} seconds")
+
+"""
+Solution found in 0.0024766921997070312 seconds
+"""
