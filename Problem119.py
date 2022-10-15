b47335590d5668e3f81afc3de69ce2bbd262c57b
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-10-15 18:36:26 +0200

    Init Problem119.py with template

---
+++ Problem119.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 15.10.2022 18:30
+
+Problem 119: Digit power sum
+https://projecteuler.net/problem=119
+
+@author: Jakub
+
+Algoritm description [Draft]:
+"""
+
+# import libraries
+import time
+
+# main solution function
+def solution():
+    pass
+
+if __name__ == "__main__":
+    # start_time = time.time()
+    solution()
+    # print(f"Solution found in {time.time() - start_time} seconds")
