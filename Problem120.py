ca389f3cd7e5b2c1efb1e39c8d6df5522e021b45
KubiakJakub01 <kubiak.jakub01@gmail.com>
2022-10-15 20:45:13 +0200

    Init Problem120 with template

---
+++ Problem120.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 15.10.2022 20:30
+
+Problem 120: Square remainders
+https://projecteuler.net/problem=120
+
+@author: Jakub
+
+Algoritm description:
+"""
+
+# import libraries
+from math import remainder
+import time
+
+# main solution function
+def solution():
+    pass
+
+if __name__ == "__main__":
+    # start_time = time.time()
+    solution()
+    # print(f"Solution found in {time.time() - start_time} seconds")
