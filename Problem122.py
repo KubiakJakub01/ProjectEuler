67b50489e0ff18cce2d82bd2c96a3cd4d28d8e78
James Kubiak <kubaszek10@gmail.com>
2022-10-16 12:39:27 +0200

    Init Problem122

---
+++ Problem122.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 16.10.2022 12:38
+Problem 122: 
+https://projecteuler.net/problem=122
+@author: Jakub
+Algoritm description [draft]:
+"""
+
+# imports libraries
+import time
+
+# main solution function
+def solution():
+    pass
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(solution())
+    print(f"Solution found in {time.time() - start_time} seconds")
