54cb001b65231dc5359c9b18dd462d9d4439a888
James Kubiak <kubaszek10@gmail.com>
2022-10-16 13:19:04 +0200

    Provide dict as level structure

--- Problem122.py
+++ Problem122.py
@@ -9,10 +9,29 @@ Algoritm description [draft]:
 
 # imports libraries
 import time
+from collections import defaultdict
 
 # main solution function
 def solution():
-    pass
+    # init variables
+    level_dict = defaultdict(list)
+    level_dict[0] = [1]
+    level_dict[1] = [2]
+    nubmers_list = [1, 2]
+    # main loop
+    for i in range(10):
+        current_level = len(level_dict) + 1
+        # numbers in current level are sum of number in previous level and number in levels before previous
+        for number in level_dict[current_level - 1]:
+            new_numbers = [x + number for level_number in range(0, current_level - 1) for x in level_dict[level_number]  if x + number not in nubmers_list]
+            print(new_numbers)
+            level_dict[current_level].extend(new_numbers)
+            nubmers_list += new_numbers
+        print(current_level, level_dict[current_level])
+    # final result is sum of level_number*number_of_numbers_in_level
+    result = sum([level_number*len(level_dict[level_number]) for level_number in level_dict])
+    #print(level_dict)
+    return result
 
 if __name__ == "__main__":
     start_time = time.time()
