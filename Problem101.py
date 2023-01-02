ed284309961c289b60be8dd8f89c0ef3ade976b7
Jakub <kubiak.jakub01@gmail.com>
2023-01-02 18:58:33 +0100

    Add correct solution for Problem124

--- Problem101.py
+++ Problem101.py
@@ -25,7 +25,8 @@ from collections import defaultdict
 # given coefficients:
 # 1 − n + n2 − n3 + n4 − n5 + n6 − n7 + n8 − n9 + n10
 COEFFICIENTS = [1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1]
-N = len(COEFFICIENTS) - 1
+TEST_COEFFICIENTS = [0, 0, 0, 1]
+N = len(TEST_COEFFICIENTS)
 
 # polunomial function
 def polynomial(x, coefficients):
@@ -37,20 +38,17 @@ def polynomial(x, coefficients):
 
 # main solution function
 def solution():
-    level_dict = defaultdict(list)
-    level_dict[0] = [1]
-    BOP = [1]
-    for x in range(2, N + 1):
-        level_dict[0].append(polynomial(x, COEFFICIENTS))
-        for key in level_dict.keys():
-            if key == 0:
-                current_val = level_dict[key][-1] - level_dict[key][-2]
-            else:
-                level_dict[key].append(current_val)
-                current_val = level_dict[key][-1] - level_dict[key][-2]
-        level_dict[len(level_dict.keys()) + 1].append(current_val)
-        bop = sum([value[-1] for value in level_dict.values()])
-        BOP.append(bop)
+    b = []
+    BOP = []    
+    for x in range(1, N+1):
+        coefficients_metrix = []
+        for n in range(1, x+1):
+            coefficients_metrix.append([n ** m for m in range(0, x)])
+        a = np.array(coefficients_metrix)
+        b.append(polynomial(x, TEST_COEFFICIENTS))        
+        X = np.linalg.solve(a, b)
+        print(X)
+        BOP.append(int(polynomial(x+1, X)))
     return BOP
 
 
---
+++ Problem124.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 02-01-2023 17:26
+
+Problem 124: Ordered radicals
+https://projecteuler.net/problem=124
+
+@author: Jakub
+"""
+
+# import libraries
+import time
+
+import numpy as np
+
+
+# main solution function
+def solution():
+    pass
+
+if __name__ == "__main__":
+    #start_time = time.time()
+    solution()
+    #print("--- %s seconds ---" % (time.time() - start_time))
