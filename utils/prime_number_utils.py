0e9ded03bce8867c11873d7536858a26dbc38be2
Jakub <kubiak.jakub01@gmail.com>
2023-01-02 19:00:28 +0100

    Add prime_number_utils

---
+++ utils/prime_number_utils.py
@@ -0,0 +1,46 @@
+"""
+Utility functions for prime numbers
+"""
+
+def is_prime(n):
+    """Return True if n is prime, False otherwise"""
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    k = 3
+    while k * k <= n:
+        if n % k == 0:
+            return False
+        k += 2
+    return True
+
+def enumerate_primes(n):
+    """Return a list of prime numbers less than n"""
+    return [x for x in range(n) if is_prime(x)]
+
+def eratos(n):
+    """Return a list of prime numbers less than n"""
+    # Sieve of Eratosthenes
+    # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    # https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+    # https://www.youtube.com/watch?v=eKp56OLhoQs
+    if n < 2:
+        return []
+    if n == 2:
+        return [2]
+    # Create a boolean array "prime[0..n]" and initialize
+    # all entries it as true. A value in prime[i] will
+    # finally be false if i is Not a prime, else true.
+    prime = [True] * n
+    p = 2
+    while p * p <= n:
+        # If prime[p] is not changed, then it is a prime
+        if prime[p]:
+            # Update all multiples of p
+            for i in range(p * 2, n, p):
+                prime[i] = False
+        p += 1
+    return [p for p in range(2, n) if prime[p]]
