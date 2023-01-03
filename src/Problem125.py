e749044dc20ff9afff96a3963b7fcb1796339a93
Jakub <kubiak.jakub01@gmail.com>
2023-01-03 18:48:33 +0100

    Init Problem125

---
+++ src/Problem125.py
@@ -0,0 +1,20 @@
+"""
+Created on 03-01-2023 18:46
+
+Problem 125: Palindromic sums
+https://projecteuler.net/problem=125
+
+@author: KubiakJakub01
+"""
+
+# import libraries
+import time
+
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    start_time = time.time()
+    solution()
+    print("--- %s seconds ---" % (time.time() - start_time))
