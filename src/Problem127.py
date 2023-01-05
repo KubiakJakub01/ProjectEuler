d86959f2655c90589c2d0b91d3a6d579d1a74a4e
Jakub <kubiak.jakub01@gmail.com>
2023-01-05 17:02:46 +0100

    Init Problem127

---
+++ src/Problem127.py
@@ -0,0 +1,23 @@
+"""
+Created on 05-01-2023 17:01
+
+Problem 127: abc-hits
+https://projecteuler.net/problem=127
+
+author: KubiakJakub01
+"""
+
+# import libraries
+import time
+
+import numpy as np
+
+N = 120000
+
+def solution():
+    pass
+
+if __name__ == "__main__":
+    start_time = time.time()
+    solution()
+    print("--- %s seconds ---" % (time.time() - start_time))
