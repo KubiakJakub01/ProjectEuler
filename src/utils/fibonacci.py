155a0deed5e6c3044235532241a85f16dcfa61c3
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-01-25 20:40:50 +0100

    Add fib function

--- src/utils/fibonacci.py
+++ src/utils/fibonacci.py
@@ -2,4 +2,27 @@
 Module for Fibonacci sequence
 """
 
-
+def fib(n: int) -> int:
+    """Return the nth Fibonacci number:
+    
+    Args:
+        n (int): The index of the Fibonacci number to return
+    
+    Returns:
+        int: The nth Fibonacci number"""
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        # Iterative solution
+        # https://en.wikipedia.org/wiki/Fibonacci_number
+        # https://www.geeksforgeeks.org/program-for-nth-fibonacci-number/
+        # https://www.youtube.com/watch?v=Qk0zUZW-U_M
+        a = 0
+        b = 1
+        for _ in range(n - 1):
+            a, b = b, a + b
+        return b
