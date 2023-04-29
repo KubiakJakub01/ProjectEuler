b03c5d8ced0b121746e1a4cb78f060aaa41dc198
Jakub <kubiak.jakub01@gmail.com>
2023-04-29 19:35:10 +0200

    Add group_numbers function

--- src/Problem89/Problem89.py
+++ src/Problem89/Problem89.py
@@ -13,13 +13,13 @@ from pathlib import Path
 roman_nums = []
 
 roman_dictonary = {
-    "M": 1000,
-    "D": 500,
-    "C": 100,
-    "L": 50,
-    "X": 10,
-    "V": 5,
-    "I": 1,
+    "M": "1000",
+    "D": "500",
+    "C": "100",
+    "L": "50",
+    "X": "10",
+    "V": "5",
+    "I": "1",
 }
 
 
@@ -32,14 +32,29 @@ def roman_to_int(roman_num):
     return [roman_dictonary[x] for x in roman_num]
 
 
-def sum_int_nums(int_nums):
-    sum = 0
-    for i in range(len(int_nums)):
-        if i + 1 < len(int_nums) and int_nums[i] < int_nums[i + 1]:
-            sum -= int_nums[i]
+def group_numbers(int_nums):
+    grouped_nums: list = []
+    len_int_nums = list(map(len, int_nums))
+    for element in sorted(set(len_int_nums), reverse=True):
+        if element in len_int_nums:
+            last_element = len(len_int_nums) - 1 - len_int_nums[::-1].index(element)
+            grouped_nums.append(int_nums[:last_element + 1])
+            int_nums = int_nums[last_element + 1:]
+            len_int_nums = len_int_nums[last_element + 1:]
+    return grouped_nums
+
+
+def valid_grouped_nums(grouped_nums):
+    def _valid_group(group):
+        if len(group) > 3:
+            return True
         else:
-            sum += int_nums[i]
-    return sum
+            return False
+    valid_grouped_nums = []
+    for group in grouped_nums:
+        if len(group) > 3:
+            valid_grouped_nums.append(group)
+    return valid_grouped_nums
 
 
 def int_to_roman(int_num):
@@ -55,10 +70,11 @@ def solution():
     file_path = Path(__file__).parent / "roman.txt"
     roman_nums = load_file(file_path)
     int_nums_list = list(map(roman_to_int, roman_nums))
-    int_nums_sum_list = list(map(sum_int_nums, int_nums_list))
-    converted_roman_nums = list(map(int_to_roman, int_nums_sum_list))
-    for roman, int_num, converted_roman in zip(roman_nums, int_nums_sum_list, converted_roman_nums):
-        print(f"orginal: {roman} int: {int_num} converted: {converted_roman}")
+    grouped_int_nums_list = list(map(group_numbers, int_nums_list))
+    for numer, grouped_int_nums in zip(roman_nums, grouped_int_nums_list):
+        print(f"orginal: {numer} grouped: {grouped_int_nums}")
+    # for roman, int_num, converted_roman in zip(roman_nums, int_nums_sum_list, converted_roman_nums):
+    #     print(f"orginal: {roman} int: {int_num} converted: {converted_roman}")
 
 
 if __name__ == "__main__":
