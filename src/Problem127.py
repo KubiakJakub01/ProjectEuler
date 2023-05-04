71eee1bc3cf1ec82e64aa71fa0e5f2ee3e9513a8
Jakub <kubiak.jakub01@gmail.com>
2023-05-04 21:18:08 +0200

    Refacor code

--- src/Problem127.py
+++ src/Problem127.py
@@ -14,27 +14,27 @@ LIMIT = 120000
 
 # main solution function
 def solution():
-	LIMIT = 120000
+    LIMIT = 120000
 
-	rads = [0] + [1] * (LIMIT - 1)
-	for i in range(2, len(rads)):
-		if rads[i] == 1:
-			for j in range(i, len(rads), i):
-				rads[j] *= i
-	
-	sortedrads = sorted((rad, n) for (n, rad) in enumerate(rads))
-	sortedrads = sortedrads[1 : ]
-	
-	ans = 0
-	for c in range(2, LIMIT):
-		for (rad, a) in sortedrads:
-			rad *= rads[c]
-			if rad >= c:
-				break
-			b = c - a
-			if a < b and rad * rads[b] < c and math.gcd(a, b) == 1:
-				ans += c
-	return str(ans)
+    rads = [0] + [1] * (LIMIT - 1)
+    for i in range(2, len(rads)):
+        if rads[i] == 1:
+            for j in range(i, len(rads), i):
+                rads[j] *= i
+
+    sortedrads = sorted((rad, n) for (n, rad) in enumerate(rads))
+    sortedrads = sortedrads[1:]
+
+    ans = 0
+    for c in range(2, LIMIT):
+        for (rad, a) in sortedrads:
+            rad *= rads[c]
+            if rad >= c:
+                break
+            b = c - a
+            if a < b and rad * rads[b] < c and math.gcd(a, b) == 1:
+                ans += c
+    return str(ans)
 
 
 if __name__ == "__main__":
