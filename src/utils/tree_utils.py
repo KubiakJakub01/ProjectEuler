61ce2c65de3f8771e9af4d367043d1da917cca47
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-13 22:24:21 +0200

    Add create_tree_from_list_of_values function

--- src/utils/tree_utils.py
+++ src/utils/tree_utils.py
@@ -1,6 +1,7 @@
 """Module with tree utilities"""
-import collections
 import typing
+import functools
+import collections
 
 from .tree_node import TreeNode
 
@@ -23,3 +24,25 @@ def create_tree_node_list_from_list_of_values(values: typing.List[typing.Any]) -
         else:
             tree_node_list.append(TreeNode(value))
     return tree_node_list
+
+
+def create_tree_from_list_of_values(values: typing.List[typing.Any]) -> TreeNode:
+    """Create a tree from a list of values
+
+    Args:
+        values (typing.List[typing.Any]): The list of values
+
+    Returns:
+        TreeNode: The root of the tree
+    """
+    if not values:
+        return None
+    tree_node_list = create_tree_node_list_from_list_of_values(values)
+    for i, tree_node in enumerate(tree_node_list):
+        if tree_node is None:
+            continue
+        if 2 * i + 1 < len(tree_node_list):
+            tree_node.left = tree_node_list[2 * i + 1]
+        if 2 * i + 2 < len(tree_node_list):
+            tree_node.right = tree_node_list[2 * i + 2]
+    return tree_node_list[0]
