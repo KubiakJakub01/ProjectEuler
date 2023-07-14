8ed72db1aebce2bb085c013bcad98b2f05d13dae
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-14 21:31:44 +0200

    Add BinaryNode class

--- src/utils/tree/node/tree_node.py
+++ src/utils/tree/node/tree_node.py
@@ -33,3 +33,34 @@ class TreeNode:
             str: The string representation of the tree node
         """
         return f"TreeNode({self.val}, {self.left}, {self.right})"
+
+
+class BinaryNode(TreeNode):
+    """Class representing a binary tree node"""
+
+    def __init__(self, val: typing.Any, left: typing.Optional["BinaryNode"] = None,
+                 right: typing.Optional["BinaryNode"] = None):
+        """Initialize a binary tree node
+
+        Args:
+            val (typing.Any): The value of the node
+            left (typing.Optional["BinaryNode"], optional): The left child of the node. Defaults to None.
+            right (typing.Optional["BinaryNode"], optional): The right child of the node. Defaults to None.
+        """
+        super().__init__(val, left, right)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the binary tree node
+
+        Returns:
+            str: The string representation of the binary tree node
+        """
+        return f"BinaryNode({self.val}, {self.left}, {self.right})"
+
+    def __str__(self) -> str:
+        """Return a string representation of the binary tree node
+
+        Returns:
+            str: The string representation of the binary tree node
+        """
+        return f"BinaryNode({self.val}, {self.left}, {self.right})"
