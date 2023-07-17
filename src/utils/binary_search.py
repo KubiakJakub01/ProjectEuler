0f635bf0e99a4104485cbe65799ae5eb00c2ca9e
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-17 21:45:28 +0200

    Add interative binary search

--- src/utils/binary_search.py
+++ src/utils/binary_search.py
@@ -39,3 +39,40 @@ def binary_search(
         return binary_search(array, value, left, middle - 1)
     else:
         return binary_search(array, value, middle + 1, right)
+
+
+def binary_search_iterative(
+    array: np.ndarray,
+    value: float,
+    left: Optional[int] = None,
+    right: Optional[int] = None,
+) -> int:
+    """Find index of the element in the sorted array.
+
+    Args:
+        array: sorted array.
+        value: value to search.
+        left: left bound of the search.
+        right: right bound of the search.
+
+    Returns:
+        index of the element in the array.
+
+    Raises:
+        ValueError: if the value is not in the array.
+    """
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array) - 1
+
+    while left <= right:
+        middle = (left + right) // 2
+        if array[middle] == value:
+            return middle
+        elif array[middle] > value:
+            right = middle - 1
+        else:
+            left = middle + 1
+
+    raise ValueError(f"Value {value} is not in the array.")
