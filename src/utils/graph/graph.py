1a54921c4af5c42e34afddbeae8bd4b94034ca05
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-19 21:55:08 +0200

    Add bfs and dfs searchs

--- src/utils/graph/graph.py
+++ src/utils/graph/graph.py
@@ -39,3 +39,39 @@ class Graph:
             adjacency_list[edge[0]].append(edge[1])
             adjacency_list[edge[1]].append(edge[0])
         return adjacency_list
+
+    def breadth_first_search(self, start: int) -> List[int]:
+        """Breadth first search algorithm.
+
+        Args:
+            start: start vertex.
+
+        Returns:
+            list of visited vertices.
+        """
+        visited = []
+        queue = [start]
+        while queue:
+            vertex = queue.pop(0)
+            if vertex not in visited:
+                visited.append(vertex)
+                queue.extend(self._adjacency_list[vertex])
+        return visited
+
+    def depth_first_search(self, start: int) -> List[int]:
+        """Depth first search algorithm.
+
+        Args:
+            start: start vertex.
+
+        Returns:
+            list of visited vertices.
+        """
+        visited = []
+        stack = [start]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.append(vertex)
+                stack.extend(self._adjacency_list[vertex])
+        return visited
