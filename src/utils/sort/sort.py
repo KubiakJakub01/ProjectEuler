7143dd82e98300c1e251d8aeeda11f031a9055f1
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-25 22:10:03 +0200

    Add quick sort algorithm implementation

---
+++ src/utils/sort/sort.py
@@ -0,0 +1,12 @@
+"""Module with sort utils"""
+import numpy as np
+
+
+def quick_sort(array):
+    """Quick sort"""
+    if len(array) < 2:
+        return array
+    pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
