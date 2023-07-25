d6a08b14f6c44cc04d8281cb9f8ab401c6602357
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-25 22:10:39 +0200

    Add merge sort alg

--- src/utils/sort/sort.py
+++ src/utils/sort/sort.py
@@ -10,3 +10,32 @@ def quick_sort(array):
     less = [i for i in array[1:] if i <= pivot]
     greater = [i for i in array[1:] if i > pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+def merge_sort(array):
+    """Merge sort"""
+    if len(array) < 2:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    """Merge"""
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result
