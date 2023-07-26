463f68d688536e166d7315ff10310e3c60c80cb5
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-26 22:09:31 +0200

    Add heap sort implementation

--- src/utils/heap/heap.py
+++ src/utils/heap/heap.py
@@ -14,3 +14,14 @@ def heapify(array, n, i):
     if largest != i:
         array[i], array[largest] = array[largest], array[i]
         heapify(array, n, largest)
+
+
+def heap_sort(array):
+    """Heap sort"""
+    n = len(array)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(array, n, i)
+    for i in range(n - 1, 0, -1):
+        array[i], array[0] = array[0], array[i]
+        heapify(array, i, 0)
+    return array
