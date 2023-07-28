0cdcda638ee1ad7b5fe397b5d14d8361387b8f34
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-07-28 21:51:21 +0200

    Create Heap class

--- src/utils/heap/heap.py
+++ src/utils/heap/heap.py
@@ -2,69 +2,80 @@
 import numpy as np
 
 
-def heapify(array: np.ndarray, n: int, i: int) -> None:
-    """Heapify
-    
-    Args:
-        array: array to heapify
-        n: size of heap
-        i: index of root"""
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-    if left < n and array[i] < array[left]:
-        largest = left
-    if right < n and array[largest] < array[right]:
-        largest = right
-    if largest != i:
-        array[i], array[largest] = array[largest], array[i]
-        heapify(array, n, largest)
+class Heap:
+    """Heap class"""
+
+    def __init__(self, array: np.ndarray) -> None:
+        """Initialize heap
+
+        Args:
+            array: array to heapify"""
+        self.array = array
+        self.heapify()
+
+    def heapify(self, array: np.ndarray, n: int, i: int) -> None:
+        """Heapify
+        
+        Args:
+            array: array to heapify
+            n: size of heap
+            i: index of root"""
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < n and array[i] < array[left]:
+            largest = left
+        if right < n and array[largest] < array[right]:
+            largest = right
+        if largest != i:
+            array[i], array[largest] = array[largest], array[i]
+            self.heapify(array, n, largest)
 
 
-def heap_sort(array: np.ndarray) -> np.ndarray:
-    """Heap sort
+    def heap_sort(self) -> np.ndarray:
+        """Heap sort
 
-    Args:
-        array: array to sort"""
-    n = len(array)
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(array, n, i)
-    for i in range(n - 1, 0, -1):
-        array[i], array[0] = array[0], array[i]
-        heapify(array, i, 0)
-    return array
-
-
-def heap_push(array: np.ndarray, value: int) -> None:
-    """Push value to heap
-
-    Args:
-        array: heap
-        value: value to push"""
-    array.append(value)
-    i = len(array) - 1
-    while i > 0 and array[i] > array[(i - 1) // 2]:
-        array[i], array[(i - 1) // 2] = array[(i - 1) // 2], array[i]
-        i = (i - 1) // 2
+        Args:
+            array: array to sort"""
+        n = len(self.array)
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapify(self.array, n, i)
+        for i in range(n - 1, 0, -1):
+            self.array[i], self.array[0] = self.array[0], self.array[i]
+            self.heapify(self.array, i, 0)
+        return self.array
 
 
-def heap_pop(array: np.ndarray) -> int:
-    """Pop value from heap
+    def heap_push(self, value: int) -> None:
+        """Push value to heap
 
-    Args:
-        array: heap"""
-    array[0], array[-1] = array[-1], array[0]
-    value = array.pop()
-    heapify(array, len(array), 0)
-    return value
+        Args:
+            array: heap
+            value: value to push"""
+        self.array.append(value)
+        i = len(self.array) - 1
+        while i > 0 and self.array[i] > self.array[(i - 1) // 2]:
+            self.array[i], self.array[(i - 1) // 2] = self.array[(i - 1) // 2], self.array[i]
+            i = (i - 1) // 2
 
 
-def heap_replace(array: np.ndarray, value: int) -> int:
-    """Replace value in heap
+    def heap_pop(self) -> int:
+        """Pop value from heap
 
-    Args:
-        array: heap
-        value: value to replace"""
-    array[0], value = value, array[0]
-    heapify(array, len(array), 0)
-    return value
+        Args:
+            array: heap"""
+        self.array[0], self.array[-1] = self.array[-1], self.array[0]
+        value = self.array.pop()
+        self.heapify(self.array, len(self.array), 0)
+        return value
+
+
+    def heap_replace(self, value: int) -> int:
+        """Replace value in heap
+
+        Args:
+            array: heap
+            value: value to replace"""
+        self.array[0], value = value, self.array[0]
+        self.heapify(self.array, len(self.array), 0)
+        return value
