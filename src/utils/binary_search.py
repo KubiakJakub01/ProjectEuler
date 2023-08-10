4ad07e20c9a877fc0cfd7a3ce7bb64217d3424a5
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-08-10 21:50:39 +0200

    Add binary_search_rightmost

--- src/utils/binary_search.py
+++ src/utils/binary_search.py
@@ -114,3 +114,41 @@ def binary_search_leftmost(
         return left
 
     raise ValueError(f"Value {value} is not in the array.")
+
+
+def binary_search_rightmost(
+    array: np.ndarray,
+    value: float,
+    left: Optional[int] = None,
+    right: Optional[int] = None,
+) -> int:
+    """Find index of the rightmost element in the sorted array.
+
+    Args:
+        array: sorted array.
+        value: value to search.
+        left: left bound of the search.
+        right: right bound of the search.
+
+    Returns:
+        index of the rightmost element in the array.
+
+    Raises:
+        ValueError: if the value is not in the array.
+    """
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array) - 1
+
+    while left < right:
+        middle = (left + right + 1) // 2
+        if array[middle] > value:
+            right = middle - 1
+        else:
+            left = middle
+
+    if array[left] == value:
+        return left
+
+    raise ValueError(f"Value {value} is not in the array.")
