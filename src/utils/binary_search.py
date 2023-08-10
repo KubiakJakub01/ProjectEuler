9aa1767df1a04f2065f0f99dc283a783dad155d7
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-08-10 21:50:16 +0200

    Add binary_search_leftmost function

--- src/utils/binary_search.py
+++ src/utils/binary_search.py
@@ -76,3 +76,41 @@ def binary_search_iterative(
             left = middle + 1
 
     raise ValueError(f"Value {value} is not in the array.")
+
+
+def binary_search_leftmost(
+    array: np.ndarray,
+    value: float,
+    left: Optional[int] = None,
+    right: Optional[int] = None,
+) -> int:
+    """Find index of the leftmost element in the sorted array.
+
+    Args:
+        array: sorted array.
+        value: value to search.
+        left: left bound of the search.
+        right: right bound of the search.
+
+    Returns:
+        index of the leftmost element in the array.
+
+    Raises:
+        ValueError: if the value is not in the array.
+    """
+    if left is None:
+        left = 0
+    if right is None:
+        right = len(array) - 1
+
+    while left < right:
+        middle = (left + right) // 2
+        if array[middle] < value:
+            left = middle + 1
+        else:
+            right = middle
+
+    if array[left] == value:
+        return left
+
+    raise ValueError(f"Value {value} is not in the array.")
