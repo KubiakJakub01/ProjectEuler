22b92ed8dbadee069a28850b07984c3f45e8cd9a
Jakub <kubiak.jakub01@gmail.com>
2023-08-13 22:48:20 +0200

    Init Network class

--- src/utils/graph/network.py
+++ src/utils/graph/network.py
@@ -2,3 +2,29 @@
 from typing import Dict, List, Optional, Set, Tuple
 
 import numpy as np
+
+
+class Network:
+    """Network implementation."""
+
+    def __init__(self, vertices: List[int], edges: List[Tuple[int, int]]):
+        """Initialize network.
+
+        Args:
+            vertices: list of vertices.
+            edges: list of edges.
+        """
+        self._vertices = vertices
+        self._edges = edges
+        self._adjacency_list = self._build_adjacency_list()
+
+    @property
+    def vertices(self) -> List[int]:
+        """Return list of vertices."""
+        return self._vertices
+
+    @property
+    def edges(self) -> List[Tuple[int, int]]:
+        """Return list of edges."""
+        return self._edges
+
