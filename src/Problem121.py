d7eb3456ac402ac68ba7d5277cebfef652659c21
Jakub <kubiak.jakub01@gmail.com>
2023-08-14 23:23:42 +0200

    Init Prbolem121.py file

---
+++ src/Problem121.py
@@ -0,0 +1,21 @@
+"""
+Created on 14-08-2023
+
+Problem 121: Disc Game Prize Fund
+https://projecteuler.net/problem=121
+
+author: KubiakJakub01
+"""
+import time
+from itertools import combinations_with_replacement
+
+
+# main solution function
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(solution())
+    print("--- %s seconds ---" % (time.time() - start_time))
