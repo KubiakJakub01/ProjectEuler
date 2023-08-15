727a562a6e6712e91d4812b1b6996d054e7b83d1
Jakub <kubiak.jakub01@gmail.com>
2023-08-15 15:39:55 +0200

    Solved problem 121

--- src/Problem121.py
+++ src/Problem121.py
@@ -6,39 +6,39 @@ https://projecteuler.net/problem=121
 
 author: KubiakJakub01
 """
+import math
 import time
 from itertools import combinations
 
-import numpy as np
-
-LIMIT = 4
+LIMIT = 15
 
 # main solution function
 def solution():
-    products = []
+    products_sum = 0
     # create list of probabilities for each turn
     probabilities = [1 / (i + 1) for i in range(1, LIMIT + 1)]
     # create list of reverse probabilities for each turn
     reverse_probabilities = [1 - i for i in probabilities]
-    # create list of all possible combinations
-    print(probabilities)
-    print(reverse_probabilities)
+    # iterate over all winning combinations
     for i in range(LIMIT // 2 + 1, LIMIT + 1):
         for comb in combinations(range(LIMIT), i):
             prob = 1
-            # create list of all possible products
+            # calculate probability of winning
             for j in range(LIMIT):
                 if j in comb:
                     prob *= probabilities[j]
                 else:
                     prob *= reverse_probabilities[j]
-            products.append(prob)
-    print(products)
-    # return sum of all products
-    return sum(products)
+            products_sum += prob
+    # return the result
+    return math.floor((1 - products_sum) / products_sum) + 1
 
 
 if __name__ == "__main__":
     start_time = time.time()
     print(solution())
     print("--- %s seconds ---" % (time.time() - start_time))
+
+"""
+--- 0.07505106925964355 seconds ---
+"""
