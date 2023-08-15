99bdf894dfc6c1376649f7a5e5fedece49022dee
Jakub <kubiak.jakub01@gmail.com>
2023-08-15 23:26:39 +0200

    Add Problem129

---
+++ src/Problem129.py
@@ -0,0 +1,36 @@
+"""
+Created on 15-08-2023
+
+Problem 129: Repunit Divisibility
+https://projecteuler.net/problem=129
+
+@author: KubiakJakub01
+"""
+import math
+import time
+
+
+def A(n):
+    # Return the smallest number k such that R(k) is divisible by n
+    k = 1
+    R = 1
+    while R != 0:
+        R = (R * 10 + 1) % n
+        k += 1
+    return k
+
+
+def solution():
+    # Find the least value of n for which A(n) first exceeds 10^6
+    n = 10**6
+    i = n + 1
+    while  A(i) <= n:
+        i += 2
+        while i % 5 == 0:
+            i += 2
+    return i
+
+if __name__ == '__main__':
+    start_time = time.time()
+    print(solution())
+    print("--- %s seconds ---" % (time.time() - start_time))
