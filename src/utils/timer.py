3e16230a59d57950036367a3f3213d64937c46fa
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-08-24 22:01:23 +0200

    Add timer function

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -1,2 +1,12 @@
 """Module with timer utils"""
 import time
+
+
+def timer(func):
+    """Timer decorator"""
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        print(f"--- {time.time() - start_time} seconds ---")
+        return result
+    return wrapper
