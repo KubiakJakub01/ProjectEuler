eb8c2af0fae5bf55fea792bdac7c3082318c9d7f
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-08-31 22:08:32 +0200

    Add PriorityQueue class

--- src/utils/queue/queue.py
+++ src/utils/queue/queue.py
@@ -25,3 +25,31 @@ class Queue:
     def size(self):
         """Return queue size."""
         return len(self._queue)
+
+
+class PriorityQueue:
+    """Priority queue class."""
+
+    def __init__(self):
+        """Initialize priority queue."""
+        self._queue = []
+
+    def enqueue(self, item):
+        """Add item to the priority queue."""
+        self._queue.append(item)
+
+    def dequeue(self):
+        """Remove item from the priority queue."""
+        max_priority = 0
+        for i in range(1, len(self._queue)):
+            if self._queue[i] > self._queue[max_priority]:
+                max_priority = i
+        return self._queue.pop(max_priority)
+
+    def is_empty(self):
+        """Check if priority queue is empty."""
+        return len(self._queue) == 0
+
+    def size(self):
+        """Return priority queue size."""
+        return len(self._queue)
