ed9ca712994adc90f85de815023d408747a1b35c
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-03 22:09:13 +0200

    Add timer with tqdm function

--- src/utils/timer.py
+++ src/utils/timer.py
@@ -1,9 +1,21 @@
 """Module with timer utils"""
 import time
 
+from tqdm import tqdm
+
 
 def timer(func):
     """Timer decorator"""
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        print(f"--- {time.time() - start_time} seconds ---")
+        return result
+    return wrapper
+
+
+def tqdm_timer(func):
+    """Timer decorator with tqdm"""
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
