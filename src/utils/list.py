64d7a6d467fb3682467686d2459edba9165df7a1
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-09-04 18:05:49 +0200

    Add generate_powers fn

--- src/utils/list.py
+++ src/utils/list.py
@@ -1,5 +1,5 @@
 """Module for list utilities."""
-from typing import List, Union
+from typing import List, Union, Generator
 
 
 def is_palindromic(n: Union[int, str]) -> bool:
@@ -12,3 +12,21 @@ def is_palindromic(n: Union[int, str]) -> bool:
         bool: True if the number is palindromic, False otherwise.
     """
     return str(n) == str(n)[::-1]
+
+
+def generate_powers(n: int) -> Generator[int]:
+    """Generate a list of powers of 2 where the sum of last two numbers is less than n.
+
+    Args:
+        n (int): Number to check.
+
+    Returns:
+        List[int]: List of powers of 2.
+    """
+    suma_last_two = 0
+    i = 1
+    while suma_last_two < n:
+        power = i**2
+        i += 1
+        suma_last_two = power + i**2
+        yield power
