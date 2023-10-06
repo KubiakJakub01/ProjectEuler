f22b90ece3593a7ece518f440020dc5c407bafdf
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-10-06 22:02:52 +0200

    Add min max normalize for 2d

--- src/utils/min_max.py
+++ src/utils/min_max.py
@@ -10,3 +10,8 @@ def min_max_normalize(array):
 def min_max_denormalize(array, min_value, max_value):
     """Min-max denormalize array."""
     return array * (max_value - min_value) + min_value
+
+
+def min_max_normalize_2d(array):
+    """Min-max normalize 2d array."""
+    return np.array([min_max_normalize(array[:, i]) for i in range(array.shape[1])]).T
