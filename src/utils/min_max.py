c2a872f0ecc4c531d6bb427cea2dbc1a56606da8
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-10-06 22:02:32 +0200

    Add min_max_denormalize function

--- src/utils/min_max.py
+++ src/utils/min_max.py
@@ -5,3 +5,8 @@ import numpy as np
 def min_max_normalize(array):
     """Min-max normalize array."""
     return (array - np.min(array)) / (np.max(array) - np.min(array))
+
+
+def min_max_denormalize(array, min_value, max_value):
+    """Min-max denormalize array."""
+    return array * (max_value - min_value) + min_value
