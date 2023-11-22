6a98a19c190674cc18b74892cc3288d150ba2918
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-11-22 23:35:47 +0100

    Add recursive version of fibonacci

--- src/utils/fibonacci.py
+++ src/utils/fibonacci.py
@@ -58,3 +58,29 @@ def fib_recursive(n: int, result_dict: dict = None) -> int:
 if __name__ == "__main__":
     n = 10
     print(f"The {n}th Fibonacci number is {fib(n)}")
+
+
+def fib_recursive(n: int, result_dict: dict = None) -> int:
+    """Return the nth Fibonacci number:
+    
+    Args:
+        n (int): The index of the Fibonacci number to return
+        result_dict (dict): A dictionary to store already calculated results
+    
+    Returns:
+        int: The nth Fibonacci number"""
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    if result_dict is None:
+        result_dict = {}
+    if n in result_dict:
+        return result_dict[n]
+    if n == 0:
+        result_dict[n] = 0
+        return 0
+    if n == 1:
+        result_dict[n] = 1
+        return 1
+    else:
+        result_dict[n] = fib_recursive(n - 1, result_dict) + fib_recursive(n - 2, result_dict)
+        return result_dict[n]
