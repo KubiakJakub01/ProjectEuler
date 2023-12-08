9ee3d60219b5c05e198b25658fe8419aa1548fae
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-12-08 22:47:44 +0100

    Add relaxed and strict wer

--- src/utils/metrics/wer.py
+++ src/utils/metrics/wer.py
@@ -1,12 +1,48 @@
+from typing import Literal
+
 import jiwer
 
 
-def wer(hyp, ref):
+def wer(hyp, ref, type: Literal["relaxed", "strict"] = "relaxed"):
     """
     Computes the Word Error Rate, defined as the edit distance between the
     two provided sentences after tokenizing to words.
     Arguments:
         hyp (string): space-separated sentence
         ref (string): space-separated sentence
+        type (string): type of WER computation. relaxed or strict
+    Returns:
+        int: the WER
     """
-    return jiwer.wer(ref, hyp)
+    if type == "relaxed":
+        transform = jiwer.Compose(
+            [
+                jiwer.ToLowerCase(),
+                jiwer.RemoveMultipleSpaces(),
+                jiwer.RemoveWhiteSpace(replace_by_space=True),
+                jiwer.RemovePunctuation(),
+                jiwer.Strip(),
+                jiwer.SentencesToListOfWords(word_delimiter=" "),
+            ]
+        )
+    elif type == "strict":
+        transform = jiwer.Compose(
+            [
+                jiwer.ToLowerCase(),
+                jiwer.RemoveMultipleSpaces(),
+                jiwer.RemoveWhiteSpace(replace_by_space=True),
+                jiwer.RemovePunctuation(),
+                jiwer.Strip(),
+                jiwer.SentencesToListOfWords(word_delimiter=" "),
+            ]
+        )
+    
+    hyp = transform(hyp)
+    ref = transform(ref)
+
+    wer = jiwer.wer(ref, hyp)
+
+    # Normalize WER
+    if wer > 1:
+        wer = 1
+    return wer
