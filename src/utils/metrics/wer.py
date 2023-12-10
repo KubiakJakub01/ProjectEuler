a622d3ff2f06bb47b8f325f3a20a39c61532e75d
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-12-10 22:49:49 +0100

    Add cer metrics

--- src/utils/metrics/wer.py
+++ src/utils/metrics/wer.py
@@ -46,3 +46,38 @@ def wer(hyp, ref, type: Literal["relaxed", "strict"] = "relaxed"):
     if wer > 1:
         wer = 1
     return wer
+
+
+def cer(hyp, ref):
+    """
+    Computes the Character Error Rate, defined as the edit distance.
+    Arguments:
+        hyp (string): space-separated sentence
+        ref (string): space-separated sentence
+    Returns:
+        int: the CER
+    """
+    transform = jiwer.Compose(
+        [
+            jiwer.ToLowerCase(),
+            jiwer.RemoveMultipleSpaces(),
+            jiwer.RemoveWhiteSpace(replace_by_space=True),
+            jiwer.RemovePunctuation(),
+            jiwer.Strip(),
+            jiwer.SentencesToListOfWords(word_delimiter=" "),
+        ]
+    )
+
+    hyp = transform(hyp)
+    ref = transform(ref)
+
+    cer = jiwer.cer(
+        ref,
+        hyp,
+        hypothesis_transform=jiwer.remove_empty_strings
+    )
+
+    # Normalize CER
+    if cer > 1:
+        cer = 1
+    return cer
