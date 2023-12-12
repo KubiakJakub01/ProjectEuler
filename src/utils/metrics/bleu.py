dce5ee6fbe0caca53f6f94242a76e4899bbddc53
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-12-12 23:10:02 +0100

    Add bleu_corpus version

--- src/utils/metrics/bleu.py
+++ src/utils/metrics/bleu.py
@@ -27,3 +27,31 @@ def bleu(hyp, ref):
     bleu = jiwer.sentence_bleu(ref, hyp)
 
     return bleu
+
+
+def bleu_corpus(hyps, refs):
+    """
+    Computes the BLEU score between a corpus of hypotheses and references.
+    Arguments:
+        hyps (list): list of hypotheses
+        refs (list): list of references
+    Returns:
+        int: the BLEU score
+    """
+    transform = jiwer.Compose(
+        [
+            jiwer.ToLowerCase(),
+            jiwer.RemoveMultipleSpaces(),
+            jiwer.RemoveWhiteSpace(replace_by_space=True),
+            jiwer.RemovePunctuation(),
+            jiwer.Strip(),
+            jiwer.SentencesToListOfWords(word_delimiter=" "),
+        ]
+    )
+
+    hyps = [transform(hyp) for hyp in hyps]
+    refs = [transform(ref) for ref in refs]
+
+    bleu = jiwer.corpus_bleu(refs, hyps)
+
+    return bleu
