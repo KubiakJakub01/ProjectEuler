d139ceae4dbf5a42d70900046a8b0b662817dbd5
Jakub <kubiak.jakub01@gmail.com>
2024-05-01 22:46:57 +0200

    Init Problem130

---
+++ src/Problem130.py
@@ -0,0 +1,21 @@
+"""
+Created on 01.05.2024
+
+Problem 130: Composites with Prime Repunit Property
+https://projecteuler.net/problem=130
+
+@author: KubiakJakub01
+
+Algorithm description:
+    TODO
+"""
+from .utils import timer
+
+
+@timer
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    solution()
