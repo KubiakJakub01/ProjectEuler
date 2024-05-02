90d56f31a7086dad8e83f002aefb514cf75655cc
Jakub <kubiak.jakub01@gmail.com>
2024-05-02 19:39:00 +0200

    Come with new solution idea

--- src/Problem130.py
+++ src/Problem130.py
@@ -9,12 +9,32 @@ https://projecteuler.net/problem=130
 Algorithm description:
     TODO
 """
-from .utils import timer
+from .utils import divisors, pnu, timer
+
+N = 25
 
 
 @timer
 def solution():
-    pass
+    sum_results = 0
+    n_results = 0
+    n = 7
+
+    while n_results < N:
+        n += 2
+        if pnu.is_prime(n) or n % 5 == 0:
+            continue
+
+        n_1 = n - 1
+        for div in divisors(n_1):
+            repunit = int("1" * div)
+            if repunit % n == 0:
+                print(f"{n=}")
+                sum_results += n
+                n_results += 1
+                break
+
+    print(sum_results)
 
 
 if __name__ == "__main__":
