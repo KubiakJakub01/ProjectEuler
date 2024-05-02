e9b4ae3bac88bca13ca2d9893433779ea676a101
Jakub <kubiak.jakub01@gmail.com>
2024-05-02 19:36:29 +0200

    Fix import order

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from . import prime_number_utils as pnu
-from .numberic import generate_powers
+from .numberic import divisors, generate_powers
 from .timer import timer
 
-__all__ = ["timer", "generate_powers", "pnu"]
+__all__ = ["divisors", "timer", "generate_powers", "pnu"]
