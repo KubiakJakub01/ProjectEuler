228f7234303fc06356c942b99115c0cda0d3f871
Jakub <kubiak.jakub01@gmail.com>
2024-05-03 23:33:41 +0200

    Problem 130 is working

--- src/Problem130.py
+++ src/Problem130.py
@@ -9,9 +9,26 @@ https://projecteuler.net/problem=130
 Algorithm description:
     TODO
 """
+from collections import deque
+
 from .utils import divisors, pnu, timer
 
 N = 25
+
+
+def is_divisible(n: str, i: int):
+    try:
+        return int(n) % i == 0
+    except ValueError:
+        n_deque = deque(n[::-1])
+        p = ""
+        while n_deque:
+            p += n_deque.pop()
+            p = str(int(p) % i)
+
+        if p == "0":
+            return True
+        return False
 
 
 @timer
@@ -27,9 +44,9 @@ def solution():
 
         n_1 = n - 1
         for div in divisors(n_1):
-            repunit = int("1" * div)
-            if repunit % n == 0:
-                print(f"{n=}")
+            repunit = "1" * div
+            if is_divisible(repunit, n):
+                print(f"{n_results=}: {n=}")
                 sum_results += n
                 n_results += 1
                 break
@@ -39,3 +56,8 @@ def solution():
 
 if __name__ == "__main__":
     solution()
+
+
+"""
+--- 8.679726123809814 seconds ---
+"""
