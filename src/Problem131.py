ee07e1c469c3f0ce8811259a9f7d8ac5aa620b1d
Jakub <kubiak.jakub01@gmail.com>
2024-05-04 10:39:00 +0200

    Init Problem131

---
+++ src/Problem131.py
@@ -0,0 +1,21 @@
+"""
+Created on 04.05.2024
+
+Problem 130: Prime Cube Partnership
+https://projecteuler.net/problem=131
+
+@author: KubiakJakub01
+
+Algorithm description:
+    TODO
+"""
+from .utils import timer
+
+
+@timer
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    solution()
