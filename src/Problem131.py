71e8cdaac3eb70e23c8c8f763052ad94f80defa3
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-05-12 22:12:50 +0200

    Add solution for Problem131

--- src/Problem131.py
+++ src/Problem131.py
@@ -9,13 +9,36 @@ https://projecteuler.net/problem=131
 Algorithm description:
     TODO
 """
-from .utils import timer
+from .utils import pnu, timer
+
+N = 10**6
+
+
+def f(x):
+    return 3 * x**2 - 3 * x + 1
 
 
 @timer
 def solution():
-    pass
+    primes = pnu.eratos(N)
+    count = 0
+    x = 1
+
+    while True:
+        p = f(x)
+        if p >= N:
+            break
+        if p in primes:
+            count += 1
+        x += 1
+
+    print(count)
 
 
 if __name__ == "__main__":
     solution()
+
+
+"""
+--- 0.34201812744140625 seconds ---
+"""
