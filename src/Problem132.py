1a6d7893d33fe1e12640028d2a43a1d7b5cb6b41
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-05-16 23:00:27 +0200

    Init Problem132

---
+++ src/Problem132.py
@@ -0,0 +1,22 @@
+"""
+Created on 16.05.2024
+
+Problem 132: Large Repunit Factors
+https://projecteuler.net/problem=132
+
+Algorithm description:
+    TODO: Write the algorithm description.
+
+@author: KubiakJakub01
+"""
+
+from .utils import timer
+
+
+@timer
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    solution()
